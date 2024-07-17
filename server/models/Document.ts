44bb12c1f902f0a2f774b3f46cff01a829d38bf6
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-07-17 18:03:43 +0530

    fix: existing endpoints that are affected

--- server/models/Document.ts
+++ server/models/Document.ts
@@ -963,7 +963,7 @@ class Document extends ParanoidModel<
         }
       }
 
-      if (!this.template && this.publishedAt && collection) {
+      if (!this.template && this.publishedAt && collection?.isActive) {
         await collection.addDocumentToStructure(this, undefined, {
           transaction,
         });
